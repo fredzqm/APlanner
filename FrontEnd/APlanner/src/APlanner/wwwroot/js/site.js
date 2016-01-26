1b79fbf29242dab674b18d5194accecb022660a2
Fred Zhang <fredzqm@gmail.com>
2016-01-26 16:23:25 -0500

    backend reformat

--- FrontEnd/APlanner/src/APlanner/wwwroot/js/site.js
+++ FrontEnd/APlanner/src/APlanner/wwwroot/js/site.js
@@ -1,1 +1,45 @@
 ﻿// Write your Javascript code.
+
+(function ($) {
+    var options = {
+        events_source: 'Schedule/Source',
+        view: 'month',
+        tmpl_path: '~/lib/boostrap-calendar/tmpls/',
+        tmpl_cache: false,
+        onAfterEventsLoad: function (events) {
+            if (!events) {
+                return;
+            }
+            var list = $('#eventlist');
+            list.html('');
+
+            $.each(events, function (key, val) {
+                $(document.createElement('li'))
+					.html('<a href="' + val.url + '">' + val.title + '</a>')
+					.appendTo(list);
+            });
+        },
+        onAfterViewLoad: function (view) {
+            $('.page-header h3').text(this.getTitle());
+            $('.btn-group button').removeClass('active');
+            $('button[data-calendar-view="' + view + '"]').addClass('active');
+        },
+    };
+
+    var calendar = $('#calendar').calendar(options);
+
+    $('.btn-group button[data-calendar-nav]').each(function () {
+        var $this = $(this);
+        $this.click(function () {
+            calendar.navigate($this.data('calendar-nav'));
+        });
+    });
+
+    $('.btn-group button[data-calendar-view]').each(function () {
+        var $this = $(this);
+        $this.click(function () {
+            calendar.view($this.data('calendar-view'));
+        });
+    });
+
+}(jQuery));
