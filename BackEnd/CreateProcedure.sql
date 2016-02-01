7362287faf49e28653660a5ecf8fe8c8bc13d703
Fred Zhang <fredzqm@gmail.com>
2016-02-01 02:18:47 -0500

    add main.sql to run all other scripts

---
+++ BackEnd/CreateProcedure.sql
@@ -0,0 +1,53 @@
+Use [APlanner]
+Go
+
+IF OBJECT_ID('ProfessorView', 'V') IS NOT NULL
+    DROP VIEW ProfessorView;
+GO
+Create View ProfessorView  As
+	Select p.PUserID, u.FName+' '+ u.LName As [ProfName] , 
+		 p.DepartID As [DepartMent]
+	From People u, Professor p
+	where u.UserID = p.PUserID;
+Go
+
+IF OBJECT_ID('StudentView', 'V') IS NOT NULL
+    DROP VIEW StudentView;
+GO
+Create View StudentView  AS
+	Select s.SUserID, u.FName+' '+ u.LName As [StudName] , 
+		s.Major , s.YR As Year
+	From People u , Student s
+	where u.UserID = s.SUserID;
+Go
+
+IF OBJECT_ID('SectionView', 'V') IS NOT NULL
+    DROP VIEW SectionView;
+GO
+Create View SectionView  As
+	Select s.SectID, s.CourseID+'-'+s.SectNum AS [Section], s.TermID, 
+		p.ProfName, c.Credit, s.EnrollNum
+	From Course c, Section s, ProfessorView p
+	where s.CourseID = c.CourseID AND s.PUserID = p.PUserID;
+Go
+
+IF OBJECT_ID('PlanView', 'V') IS NOT NULL
+    DROP VIEW PlanView;
+GO
+
+Create View PlanView As
+	Select p.PID, s.SUserID, s.StudName, p.TermID, p.Probability, SUM(c.Credit) As [Credits]
+	From SPlan p, StudentView s, Contain l, Course c
+	where  p.SUserID = s.SUserID AND p.PID = l.PID 
+		AND l.CourseID = c.CourseID
+	Group By p.PID, s.SUserID, s.StudName, p.TermID, p.Probability
+Go
+
+IF OBJECT_ID('ScheduleView', 'V') IS NOT NULL
+    DROP VIEW ScheduleView;
+GO
+Create View SchedulePublicView As
+	Select p.SUserID, p.StudName, p.TermID, p.Probability * s.Probability As [Probability], p.Credits
+	From Schedule s, PlanView p
+	where  s.PID = p.PID AND s.PublicOrPrivate = 1;
+Go
---
+++ BackEnd/main.sql
@@ -0,0 +1,10 @@
+go
+:r C:\Users\zhang\Documents\GitHub\databaseGit\database\BackEnd\CreateDatabase.sql
+go
+:r C:\Users\zhang\Documents\GitHub\databaseGit\database\BackEnd\CreateView.sql
+go
+:r C:\Users\zhang\Documents\GitHub\databaseGit\database\BackEnd\CreateTrigger.sql
+go
+:r C:\Users\zhang\Documents\GitHub\databaseGit\database\BackEnd\InsertData.sql
+go
+:r C:\Users\zhang\Documents\GitHub\databaseGit\database\BackEnd\CreateProcedure.sql
