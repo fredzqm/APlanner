54449f81bb7a80a1938fbfc4a17c74c2fcb11e46
YukariDyy <you@example.com>
2016-02-01 16:45:51 -0500

    safiewhfoihef

--- BackEnd/InsertData.sql
+++ BackEnd/InsertData.sql
@@ -1,6 +1,8 @@
 use APlanner
+GO
 
-Insert into People
+
+Insert into People ([UserID] ,[UserName] ,[FName] ,[LName] ,[SOP] ,[Password])
 Values 
 ('801200000', 'dingy2', 'Mercury', 'Ding', 'S','pskeidjnsrkk203'),
 ('801200001', 'whitec', 'Crystal', 'White','S', 'skenskesdf3203'),
@@ -9,7 +11,7 @@ Values
 ('801200004', 'zhangq2', 'Fred', 'Zhang', 'S','pskeidjnsrkk203'),
 ('801200005', 'karryc', 'Crystal', 'Karry','P', 'skenskesdf3203');
 
-Insert into Student
+Insert into Student ([SUserID], [Major] ,[YR])
 Values 
 ('801200000', 'CS',2),
 ('801200001', 'BE',2),
@@ -17,11 +19,11 @@ Values
 ('801200003', 'SE',2),
 ('801200004', 'CPE',2);
 
-Insert into Professor
+Insert into Professor ([PUserID], [DepartID])
 Values 
 ('801200005', 'CS');
 
-Insert into Course
+Insert into Course ([CourseID] ,[CourseDP] ,[CourseNum] ,[Descrip] ,[Credit])
 Values 
 (304, 'CSSE','PLC', 4),
 (111, 'CHEM','General Chemistry', 4),
@@ -30,25 +32,24 @@ Values
 (3333, 'MA112','CalculusII', 5),
 (3335, 'MA111','CalculusI', 5);
 
-Insert into Prerequisite
-Values 
-(3333, 3335);
-
-Insert into Term
+Insert into Term ([TermID] ,[Start_Date] ,[End_Date])
 Values 
 (1,'03-07-2016','05-27-2016');
 
-Insert into Section
+Insert into Section ([SectID] ,[TermID] ,[CourseID] ,[SectNum] ,[PUserID] ,[EnrollNum] ,[Capacity])
 Values 
 (3295,1,123,1,'801200005',1,24),
 (3296,1,123,2,'801200005',0,24),
 (3297,1,123,3,'801200005',0,24);
 
-Insert into STime
+Insert into STime ([SectID] ,[Classroom] ,[Period] ,[TermID])
 Values 
 (3295,'M105',1,1);
 
-Insert into Enroll
+USE [APlanner]
+GO
+
+Insert into Enroll ([SectID], [SUserID], [T], [S], [Rating])
 values
 (3295, '801200000', '02-01-2016', 'Register', 1),
 (3295, '801200001', '02-01-2016', 'Waitlist', 1);
--- BackEnd/PROC/UserLogin.sql
+++ BackEnd/PROC/UserLogin.sql
@@ -2,11 +2,11 @@ Use APlanner
 Go
 
 Create Procedure UserLogin
-	@User varchar(15),
+	@User varchar(30),
 	@Pass varchar(30),
 	@result Bit OUTPUT
 AS
-	if (@Pass=(select Password From People where UserID=@User))
+	if (@Pass=(select Password From People where UserName=@User)) or (@Pass=(select Password From People where UserName+'.rose-hulman.edu'=@User))
 		set @result=1
 	else set @result=0
 GO
