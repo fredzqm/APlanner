4d50dcbe3ae6c37b5409cb51fd676eb84c4b8df4
Fred Zhang <fredzqm@gmail.com>
2016-02-10 14:59:38 -0500

    clean up

--- BackEnd/Encryption.sql
+++
@@ -1,8 +0,0 @@
-SELECT  DATALENGTH(HASHBYTES('MD2', 'Testing')) AS [MD2Length],
-        DATALENGTH(HASHBYTES('MD4', 'Testing')) AS [MD4Length],
-        DATALENGTH(HASHBYTES('MD5', 'Testing')) AS [MD5Length],
-        DATALENGTH(HASHBYTES('SHA', 'Testing')) AS [SHALength],
-        DATALENGTH(HASHBYTES('SHA1', 'Testing')) AS [SHA1Length],
-        /* 2012 only: */
-        DATALENGTH(HASHBYTES('SHA2_256', 'Testing')) AS [SHA2_256Length],
-        DATALENGTH(HASHBYTES('SHA2_512', 'Testing')) AS [SHA2_512Length];
